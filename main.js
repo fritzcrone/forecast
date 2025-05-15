a2919877bcb7fff39fbc72f876a366accf232fff
fritzcrone <fritz.crone@gmail.com>
2025-05-15 17:46:04 +0200

    timestamp eingefügt

--- main.js
+++ main.js
@@ -42,15 +42,17 @@ async function showForecast(latlng) {
 
     // Popup erzeugen
     let details = jsondata.properties.timeseries[0].data.instant.details;
+    let timestamp = new Date(jsondata.properties.meta.updated_at);
     let markup = `
-<ul>
-    <li>Luftdruck (hPa): ${details.air_pressure_at_sea_level}</li>
-    <li>Lufttemperatur (C°): ${details.air_temperature}</li>
-    <li>Bewölkungsgrad (%): ${details.cloud_area_fraction}</li>
-    <li>Luftfeuchtigkeit (%): ${details.relative_humidity}</li>
-    <li>Windrichtung (°): ${details.wind_from_direction}</li>
-    <li>Windgeschwindigkeit (km/h): ${details.wind_speed}</li>
-</ul>
+    <h3> Wettervorhersage für ${timestamp.toLocaleString()}</h3>
+    <ul>
+        <li>Luftdruck (hPa): ${details.air_pressure_at_sea_level}</li>
+        <li>Lufttemperatur (C°): ${details.air_temperature}</li>
+        <li>Bewölkungsgrad (%): ${details.cloud_area_fraction}</li>
+        <li>Luftfeuchtigkeit (%): ${details.relative_humidity}</li>
+        <li>Windrichtung (°): ${details.wind_from_direction}</li>
+        <li>Windgeschwindigkeit (km/h): ${details.wind_speed}</li>
+    </ul>
 `;
 
     L.popup([
